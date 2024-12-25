649a43dbde994b3b86f9c65a9926bc77c56badd5
jinseok9338 <jinseok9338@gmail.com>
2024-12-25 15:00:06 +0900

    add huskty

--- scripts/svgIconUpdate/svgIconUpdate.ts
+++ scripts/svgIconUpdate/svgIconUpdate.ts
@@ -1,20 +1,23 @@
-import path from 'path';
-import { Plugin } from 'vite';
-import svgIndexGenerate from './svnIndexGenerate';
+import path from "path";
+import type { Plugin } from "vite";
+import svgIndexGenerate from "./svnIndexGenerate";
 
 export default function svgIconUpdate(svgPath: string): Plugin {
   return {
-    name: 'svg-icon-update',
-    enforce: 'pre',
+    name: "svg-icon-update",
+    enforce: "pre",
     configureServer(server) {
-      const listener = (file = '') => {
-        if (file.includes(path.normalize(svgPath)) && path.extname(file) === '.svg') {
+      const listener = (file = "") => {
+        if (
+          file.includes(path.normalize(svgPath)) &&
+          path.extname(file) === ".svg"
+        ) {
           svgIndexGenerate(svgPath);
         }
       };
-      server.watcher.on('add', listener);
-      server.watcher.on('change', listener);
-      server.watcher.on('unlink', listener);
+      server.watcher.on("add", listener);
+      server.watcher.on("change", listener);
+      server.watcher.on("unlink", listener);
     },
     buildStart(): Promise<void> {
       svgIndexGenerate(svgPath);
