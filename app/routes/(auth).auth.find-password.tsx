150edd937a27a8af6472def7e74bb3870f075e4a
jinseok9338 <jinseok9338@gmail.com>
2024-12-28 14:18:21 +0900

    make template

---
+++ app/routes/(auth).auth.find-password.tsx
@@ -0,0 +1,7 @@
+import PasswordChangePage from "~/pages/auth/change-password";
+
+const FindPasswordIndexPage = () => {
+  return <PasswordChangePage />;
+};
+
+export default FindPasswordIndexPage;
